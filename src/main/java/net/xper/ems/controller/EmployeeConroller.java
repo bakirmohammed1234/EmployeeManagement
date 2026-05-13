644e269b0d5df88f27be3326e792cbd2a8235487
Bakir <bakirmohammed018@gmail.com>
2026-05-13 23:48:43 +0100

    bulild add employee

---
+++ src/main/java/net/xper/ems/controller/EmployeeConroller.java
@@ -0,0 +1,27 @@
+package net.xper.ems.controller;
+
+
+import lombok.AllArgsConstructor;
+import net.xper.ems.dto.EmployeeDto;
+import net.xper.ems.service.EmployeeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeConroller {
+    private EmployeeService employeeService;
+
+
+    // build add Employee Rest API
+    @PostMapping
+    public ResponseEntity<EmployeeDto> createEmployee(@RequestBody EmployeeDto employeeDto) {
+        EmployeeDto savedEmployee = employeeService.createEmployee(employeeDto);
+
+        return  new  ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
+    }
+}
